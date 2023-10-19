6eb196d1b7b752cc6fc96aafdcd20c0efbd1f6d8
tenjo-t <tenjo0314takashi@gmail.com>
2023-10-19 14:55:05 +0900

    フィボナッチ数列を自動で生成する

--- src/ylab_qc_1d_diffraction/main.py
+++ src/ylab_qc_1d_diffraction/main.py
@@ -3,18 +3,23 @@ from cmath import exp, pi, sqrt
 import matplotlib.pyplot as plt
 
 tau = (1 + sqrt(5)) / 2
-r = [
-    (0, 0),
-    (1, 0),
-    (0, 1),
-    (2, 0),
-    (1, 1),
-    (3, 0),
-    (0, 2),
-]
+fib = [(0, 0), (0, 1), (1, 1)]
 
 
 def F(n):
+    r = []
+    if n > 0:
+        r.append(fib[0])
+    if n > 1:
+        r.append(fib[1])
+    if n > 2:
+        r.append(fib[2])
+    if n > 3:
+        for i in range(n - 3):
+            _1 = r[i]
+            _2 = r[i + 1]
+            r.append((_1[0] + _2[0], _1[1] + _2[1]))
+
     def F(k):
         f = 0
         for i in range(n):
@@ -32,7 +37,7 @@ def F(n):
 if __name__ == "__main__":
     x = []
     y = []
-    f = F(7)
+    f = F(10)
 
     for i in range(200):
         _x = i * 4 * 0.005
